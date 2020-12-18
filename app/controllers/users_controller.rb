d89ab455871a923a22584e3bfc76a838a8273db2
Misori-simon <misorisimon@gmail.com>
2020-12-18 16:37:47 +0100

    add edit method and form for the users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,24 @@ class UsersController < ApplicationController
             render :new
         end
     end
+    def edit
+        @user = User.find(params[:id])
+        # if @user.save
+        #     redirect_to new_user_path
+        # else
+        #     render :edit
+        # end
+    end
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        # if @user.save
+        #     redirect_to :sample
+        # else
+        #     render :edit
+        # end
+        redirect_to edit_user_path
+    end
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
